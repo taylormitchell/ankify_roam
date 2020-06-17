febaed4f1458de037c0908d17339f194af9e27dd
Taylor Mitchell <taylor.mitchell@ibm.com>
2020-06-16 22:12:21 -0400

    Convert ankify function into a class

--- ankify_roam/ankifier.py
+++ ankify_roam/ankifier.py
@@ -11,50 +11,54 @@ from ankify_roam.model_templates import ROAM_BASIC, ROAM_CLOZE
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def ankify(pyroam, deck="Default", basic_model="Roam Basic", cloze_model="Roam Cloze", tag_ankify="anki_note", pageref_cloze="outside"):
-    logger.info("Fetching blocks to ankify")
-    anki_blocks = pyroam.query(lambda b: tag_ankify in b.get_tags(inherit=False))
+class Ankifier:
+    def __init__(self, deck="Default", basic_model="Roam Basic", cloze_model="Roam Cloze", tag_ankify="anki_note", pageref_cloze="outside"):
+        self.deck = deck
+        self.basic_model = basic_model
+        self.cloze_model = cloze_model
+        self.tag_ankify = tag_ankify
+        self.pageref_cloze = pageref_cloze
+        self.field_names = {}
 
-    field_names = {}
-    try:
-        basic_fields = anki.get_field_names(basic_model)
-        cloze_fields = anki.get_field_names(cloze_model)
-    except anki.ModelNotFoundError as e:
-        raise anki.ModelNotFoundError(
-            f"'{basic_model}' or '{cloze_model}' not in Anki. "\
-            "Try running `setup_anki.py` then pass 'Roam Basic' and 'Roam Cloze' "\
-            "to default_basic and default_cloze")
+    def ankify(self, pyroam): 
+        logger.info("Fetching blocks to ankify")
+        blocks_to_ankify = pyroam.query(lambda b: self.tag_ankify in b.get_tags(inherit=False))
 
-    logger.info("Converting blocks to anki notes")
-    anki_notes = []
-    for block in anki_blocks:
-        try:
-            anki_notes.append(AnkiBlock.from_block(
-                block, deck, basic_model, cloze_model, basic_fields, cloze_fields))
-        except Exception as e:
-            logging.exception(f"Failed to convert block '{block.uid}' to an AnkiBlock")
+        logger.info("Converting blocks to anki notes")
+        anki_notes = []
+        for block in blocks_to_ankify:
+            try:
+                anki_notes.append(self.block_to_anki_note(AnkiBlock(block)))
+            except:
+                logging.exception(f"Failed to convert block uid='{block.uid}' into an anki note")
 
-    logger.info("Preparing for upload to anki")
-    anki_dicts = []
-    for an in anki_notes:
-        try:
-            anki_dicts.append(an.to_dict(pageref_cloze=pageref_cloze))
-        except Exception as e:
-            logging.exception(f"Failed to convert AnkiBlock '{block.uid}' to a dictionary")
+        logger.info("Uploading to anki")
+        for anki_dict in anki_dicts:
+            try:
+                anki.upload(anki_dict)
+            except:
+                logging.exception(f"Failed to upload '{block.uid}' to anki")
 
-    logger.info("Uploading to anki")
-    for anki_dict in anki_dicts:
-        try:
-            anki.upload(anki_dict)
-        except:
-            logging.exception(f"Failed to upload '{block.uid}' to anki")
+    def block_to_anki_note(self, block):
+        field_names = {}
+        default_model = self.basic_model if block.type=="basic" else self.cloze_model
+        modelName = block.model or default_model
+        deckName = block.deck or default_deck
+        if modelName not in self.field_names.keys():
+            field_names[modelName] = anki.get_field_names(modelName)
+        fields = block.create_fields(field_names[modelName])
+        tags = block.tags()
+        return {
+            "deckName": deckName,
+            "modelName": modelName,
+            "fields": fields,
+            "tags": tags
+        }
 
-
-def ankify_from_file(path, **kwargs):
-    logger.info("Loading PyRoam")
-    pyroam = PyRoam.from_path(path)
-    ankify(pyroam, **kwargs)
-
+    def ankify_from_file(self, path):
+        logger.info("Loading PyRoam")
+        pyroam = PyRoam.from_path(path)
+        ankify(pyroam)
 
 def setup_models(overwrite=False):
     modelNames = anki.get_model_names()
@@ -70,52 +74,40 @@ def setup_models(overwrite=False):
                     "If you want to overwrite it, set `overwrite=True`")
 
 class AnkiBlock:
-    def __init__(self, type, fields, deck, uid="", tags=[]):
-        self.type = type
-        self.fields = fields
-        self.uid = uid
-        self.deck = deck
-        self.roam_tags = tags
+    def __init__(self, block, type="", deck=""):
+        self.block = block
 
-    def get_tags(self):
-        return [re.sub("\s","_",tag) for tag in self.roam_tags]
+    @property
+    def tags(self): return [re.sub("\s","_",tag) for tag in self.block.get_tags()]
+    @property
+    def uid(self): return self.block.uid
 
-    def to_dict(self, **kwargs):
-        fields = {}
-        for i, (field_name, field) in enumerate(self.fields.items()):
-            proc_cloze = True if i==0 else False
-            fields[field_name] = field.to_html(proc_cloze=proc_cloze, **kwargs) if field else ""
-        if "uid" in self.fields.keys():
-            fields["uid"] = self.uid
-        return {
-            "deckName": self.deck,
-            "modelName": self.type,
-            "fields": fields,
-            "tags": self.get_tags()
-        }
-
-    @staticmethod
-    def is_block_cloze(block):
-        return any([type(obj)==Cloze for obj in block.content])
-
-    @classmethod
-    def from_block(cls, block, deck, basic_model, cloze_model, basic_fields, cloze_fields):
-        """
-        Args:
-            block (BlockObject): 
-            deck (str): Deck
-            basic_model (str): Name of card type to upload basic cards to  
-            cloze_model (str): Name of card type to upload cloze cards to  
-            basic_fields (list of str): Field names of the basic card type
-            cloze_fields (list of str): Field names of the cloze card type
-        """
-        if cls.is_block_cloze(block):
-            type = cloze_model
-            fields = {n:f for n,f in zip_longest(
-                cloze_fields, [block], fillvalue="")}
+    @property
+    def type(self):
+        # TODO: extract type from tags
+        if any([type(obj)==Cloze for obj in self.block.content]):
+            return "cloze"
         else:
-            type = basic_model
-            fields = {n:f for n,f in zip_longest(
-                basic_fields, [block, block.children], fillvalue="")}
-        return cls(type, fields, deck, block.get("uid"), block.get_tags())
+            return "basic"
 
+    def model(self):
+        # TODO: extract model name from tags
+        return ""
+
+    @property
+    def deck(self): 
+        # TODO: extract deck name from tags
+        return ""
+
+    def create_fields(self, field_names, **kwargs):
+        if self.type=="basic":
+            blocks = [self.block, self.block.children] 
+        else:
+            blocks = [self.block]
+        fields = {}
+        for i, (fn, block) in enumerate(zip_longest(field_names, blocks)):
+            kwargs["proc_cloze"] = i==0
+            fields[fn] = block.to_html(**kwargs) if block else "" 
+        if "uid" in field_names:
+            fields["uid"] = self.uid
+        return fields
