65d7b6e92d10e06725de82c2a8d98b40ac7d8745
Taylor Mitchell <taylor.mitchell@ibm.com>
2021-04-04 12:05:07 -0400

    Remove duplicate function in util

--- ankify_roam/util.py
+++ ankify_roam/util.py
@@ -1,6 +1,7 @@
 import inspect
 import anki
 
+
 def get_default_args(func):
     signature = inspect.signature(func)
     return {
@@ -9,19 +10,6 @@ def get_default_args(func):
         if v.default is not inspect.Parameter.empty
     }
 
-
-def add_default_models():
-    modelNames = anki.get_model_names()
-    for model in [ROAM_BASIC, ROAM_CLOZE]:
-        if not model['modelName'] in modelNames:
-            anki.create_model(model)
-        else:
-            if overwrite:
-                anki.update_model(model)
-            else:
-                logging.info(
-                    f"'{model['modelName']}' already in Anki. "\
-                    "If you want to overwrite it, set `overwrite=True`")
 
 if __name__=="__main__":
     add_default_models()
