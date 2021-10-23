16f44e7ab959c79fd7476b39a07c91333f1ffe3f
Taylor Mitchell <taylor.mitchell@ibm.com>
2021-10-22 21:32:23 -0400

    Clean script

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -8,7 +8,6 @@ VERSION_FILE = "ankify_roam/_version.py"
 # Get new version from changelog
 with open(CHANGELOG) as f:
     changelog = f.read()
-pat_version="##\s*(\d+\.\d+\.\d+)"
 res = re.search("(?##\s*)\d+\.\d+\.\d+", changelog)
 if res:
     new_version = res.group()
@@ -20,10 +19,6 @@ res = subprocess.run(["git", "tag"], capture_output=True)
 existing_versions = res.stdout.decode().split()
 if "v"+new_version in existing_versions:
     raise ValueError(f"New version {new_version} already exists")
-
-# Get new version's changes
-res = re.split("##\s*\d+\.\d+\.\d+.*", changelog)
-changes = res[1].strip()
 
 # Update version file
 with open(VERSION_FILE, "w") as f:
